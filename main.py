f6ea6cad6584c85422835e715b0a9aec9b85c6f4
Felipe Martins <felipe.martins@edu.unifil.br>
2024-11-25 22:33:30 -0300

    Hotfix: mensagem corrigida na função

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def greet():
-    print("Welcome to the Hello World project!")
+    print("Bem vindo!")
 
 if __name__ == "__main__":
     print("Hello, World!")
     greet()
-    
+    git add main.py
